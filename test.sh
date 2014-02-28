db828488c5fc260400ba4bc50b84c2eceb6e4347
Christian Speich <christian@spei.ch>
2014-02-28 23:37:17 +0100

    Run tests agains simulator.

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./configure.rb -b test || exit 1
+./configure.rb -b test -t cortex-m0p-sim || exit 1
 ninja || exit 1
 make -C tools/simulator || exit 1
 tools/simulator/simulator -f firmware.elf
