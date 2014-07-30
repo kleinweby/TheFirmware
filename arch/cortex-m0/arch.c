5bd54e74fc56a6831e9d704c49c443b3ca22913c
Christian Speich <christian@spei.ch>
2014-07-30 21:27:53 +0200

    Preliminal irq_enable/disable support for lpc11xx

--- arch/cortex-m0/arch.c
+++ arch/cortex-m0/arch.c
@@ -249,3 +249,31 @@ static void arch_handle_pendsv(void)
   // We'll never get here
   unreachable();
 }
+
+typedef struct
+{
+  volatile uint32_t ISER[1];                 /*!< Offset: 0x000 (R/W)  Interrupt Set Enable Register           */
+       uint32_t RESERVED0[31];
+  volatile uint32_t ICER[1];                 /*!< Offset: 0x080 (R/W)  Interrupt Clear Enable Register          */
+       uint32_t RSERVED1[31];
+  volatile uint32_t ISPR[1];                 /*!< Offset: 0x100 (R/W)  Interrupt Set Pending Register           */
+       uint32_t RESERVED2[31];
+  volatile uint32_t ICPR[1];                 /*!< Offset: 0x180 (R/W)  Interrupt Clear Pending Register         */
+       uint32_t RESERVED3[31];
+       uint32_t RESERVED4[64];
+  volatile uint32_t IPR[8];                  /*!< Offset: 0x3EC (R/W)  Interrupt Priority Register              */
+}  NVIC_Type;
+
+#define SCS_BASE            (0xE000E000UL)                            /*!< System Control Space Base Address */
+#define NVIC_BASE           (SCS_BASE +  0x0100UL)      
+#define NVIC                ((NVIC_Type *)          NVIC_BASE)        /*!< NVIC configuration struct         */
+
+void irq_enable(uint8_t irq_number)
+{
+  NVIC->ISER[0] = (1 << ((uint32_t)(irq_number) & 0x1F));
+}
+
+void irq_disable(uint8_t irq_number)
+{
+  NVIC->ICER[0] = (1 << ((uint32_t)(irq_number) & 0x1F));
+}
