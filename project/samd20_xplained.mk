2d18ad115b065b2daa12ff6d5f74ac88f85f640e
Christian Speich <christian@spei.ch>
2015-02-21 08:19:01 +0100

    Moved samd20_xplained cmd in correct folder.

--- project/samd20_xplained.mk
+++ project/samd20_xplained.mk
@@ -1,6 +1,3 @@
 LOCAL_DIR := $(GET_LOCAL_DIR)
 
 TARGET := samd20_xplained
-
-MODULES += \
-	apps/samd20_xplained_test
--- target/samd20_xplained/rules.mk
+++ target/samd20_xplained/rules.mk
@@ -1,3 +1,13 @@
 LOCAL_DIR := $(GET_LOCAL_DIR)
 
 PLATFORM := samd20
+
+MODULE := $(LOCAL_DIR)
+
+MODULE_SRCS := \
+	$(LOCAL_DIR)/test_cmd.c
+
+MODULE_DEPS := \
+	lib/console
+
+include make/module.mk
